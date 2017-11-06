a27057953f2369ecd925de0be8f30aa0f1e38d6b
Qiu Yu <qiuyu@ebaysf.com>
2017-11-06 16:46:05 -0700

    Use same release script as in elb project

--- hack/release.sh
+++ hack/release.sh
@@ -17,11 +17,16 @@ export GITHUB_USER="${GITHUB_USER:-ufes-dev}"
 export GITHUB_REPO="${GITHUB_REPO:-seesaw}"
 
 git tag $1
+
+# tag before make to ensure version info entered into binary
+make
+
 git push --tags
 sleep 5
+
 github-release info -u $GITHUB_USER -r $GITHUB_REPO || /bin/true
-sleep 5
 github-release release -u $GITHUB_USER -r $GITHUB_REPO -t $1
+sleep 5
 
 for cmd in seesaw_ncc seesaw_healthcheck healthcheck_test_tool; do
     github-release upload -u $GITHUB_USER -r $GITHUB_REPO -t $1 --name $cmd --file _output/$cmd
